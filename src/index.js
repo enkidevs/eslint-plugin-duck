ed8bdd12013982f972c669ec830e388748b48b98
Mathieu Dutour <mathieu@dutour.me>
2016-02-01 13:41:54 +0000

    export new rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 module.exports = {
   rules: {
+    'duplicate-action-definition': require('./rules/duplicate-action-definition'),
+    'meta-validation': require('./rules/meta-validation'),
+    'missing-creator': require('./rules/missing-creator'),
     'props-order': require('./rules/props-order')
   },
   rulesConfig: {
+    'duplicate-action-definition': 0,
+    'meta-validation': 0,
+    'missing-creator': 0,
     'props-order': 0
   }
 }
