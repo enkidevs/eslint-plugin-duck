b7e4694fb7e91730be5aec825ef6be48d05bca13
Mathieu Dutour <mathieu@dutour.me>
2016-03-23 16:26:24 +0000

    build

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-export default {
-  rules: {
-    'duplicate-action-definition': require('./rules/duplicate-action-definition'),
-    'meta-validation': require('./rules/meta-validation'),
-    'missing-creator': require('./rules/missing-creator'),
-    'props-order': require('./rules/props-order'),
-    'fsa-compliant': require('./rules/fsa-compliant'),
-    'no-type-in-creator': require('./rules/no-type-in-creator')
-  },
-  rulesConfig: {
-    'duplicate-action-definition': 0,
-    'meta-validation': 0,
-    'missing-creator': 0,
-    'props-order': 0,
-    'fsa-compliant': 0,
-    'no-type-in-creator': 0
-  },
-  configs: {
-    'recommended': require('../config/recommended')
-  }
+export const rules = {
+  'duplicate-action-definition': require('./rules/duplicate-action-definition'),
+  'meta-validation': require('./rules/meta-validation'),
+  'missing-creator': require('./rules/missing-creator'),
+  'props-order': require('./rules/props-order'),
+  'fsa-compliant': require('./rules/fsa-compliant'),
+  'no-type-in-creator': require('./rules/no-type-in-creator')
+}
+
+export const configs = {
+  'recommended': require('../config/recommended')
 }
