2c6f9e21bec34f17aca82f74198bb6c0f670628f
Mathieu Dutour <mathieu@dutour.me>
2016-03-23 14:44:14 +0000

    add recommended config

---
+++ config/recommended.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['duck'],
+  rules: {
+    'duck/props-order': 2,
+    'duck/missing-creator': 2,
+    'duck/duplicate-action-definition': 2,
+    'duck/meta-validation': 0,
+    'duck/fsa-compliant': 2,
+    'duck/no-type-in-creator': 2
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ export default {
     'duplicate-action-definition': require('./rules/duplicate-action-definition'),
     'meta-validation': require('./rules/meta-validation'),
     'missing-creator': require('./rules/missing-creator'),
-    'props-order': require('./rules/props-order')
+    'props-order': require('./rules/props-order'),
+    'fsa-compliant': require('./rules/fsa-compliant'),
+    'no-type-in-creator': require('./rules/no-type-in-creator')
   },
   rulesConfig: {
     'duplicate-action-definition': 0,
     'meta-validation': 0,
     'missing-creator': 0,
-    'props-order': 0
+    'props-order': 0,
+    'fsa-compliant': 0,
+    'no-type-in-creator': 0
+  },
+  configs: {
+    'errors': require('../config/errors'),
+    'warnings': require('../config/warnings')
   }
 }
